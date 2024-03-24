d6b89f02b9ec8df6fc5df25eec17b2988309b2ab
SyedaShafaq <shafaqfatima161@gmail.com>
2024-03-24 20:34:06 +0500

    CLI based num-guessing game

---
+++ Index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+//This is CLI based number guessing game
+//Start with importing inquirer
+import inquirer from "inquirer";
+const guessingNum = Math.floor(Math.random() * 10 + 1);
+const guessingAns = await inquirer.prompt([
+    {
+        name: "userGuessedNum",
+        type: "number",
+        message: "Please guess a number b/w 1-10",
+    }
+]);
+//Use If & Else statement
+if (guessingAns.userGuessedNum === guessingNum) {
+    console.log(`Congratulations! You guessed a correct number`);
+}
+else {
+    console.log(`Sorry! You guessed a wrong number`);
+}
